47e8b6e7327daf35eb283d053e0d91f79ed15386
zsben2 <zhao_chongbin@yeah.net>
2024-06-27 21:49:50 +0800

    更改可执行程序文件名

--- c/build.bat
+++ c/build.bat
@@ -1,8 +1,8 @@
 echo "正在进行动态编译"
-gcc -o "./output/gmp计算Si（动态）" "./src/gmp计算Si.c" -lgmp
-gcc -o "./output/gmp计算T（动态）" "./src/gmp计算T.c" -lgmp
+gcc -o "./output/gmp_Si_dynamic" "./src/gmp计算Si.c" -lgmp
+gcc -o "./output/gmp_T_dynamic" "./src/gmp计算T.c" -lgmp
 echo "正在进行静态编译"
-gcc -o "./output/gmp计算Si（静态）" "./src/gmp计算Si.c" -L./lib -lgmp
-gcc -o "./output/gmp计算T（静态）" "./src/gmp计算T.c" -L./lib -lgmp
+gcc -o "./output/gmp_Si_static" "./src/gmp计算Si.c" -L./lib -lgmp
+gcc -o "./output/gmp_T_static" "./src/gmp计算T.c" -L./lib -lgmp
 del "./output/integer.txt"
 pause
