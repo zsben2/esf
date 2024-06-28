7e3e73a6efc4a15a4116072d7b7636d502c9362f
zsben2 <zhao_chongbin@yeah.net>
2024-06-28 12:44:34 +0800

    v1.7代码 从本版本考虑程序的中断继续，为后续拆解为多进程执行任务做铺垫。算法通过保存各级 `S1_1`, 以便从任意 n 开始重新运行，并保存出为整数的 `S[n][i][k]` 。在初始状态中, `S1_1` 表示 `S[1][*][1]` 。
    补充 S1_1 缓存文件 `n_S1_1_cache.bin` 的说明

--- c/src/gmp计算Si.c
+++ c/src/gmp计算Si.c
@@ -1,20 +1,18 @@
 #include "tools.h"
 
-void check_solution_S(mpq_t**);
+void check_solution_S_with_cache(mpq_t**, const char*);
+mpq_t *read_nth_S1_1(const char*, int*);
+void save_nth_S1_1(const int, const mpq_t*, const char*);
 
 int main() {
     clock_t tk[4] = {clock()};
     mpq_t ***S;
-    logging("当前算法仅开放一行 i 的空间滚动使用，并放弃使用一个闲置空间\n");
-    logging("去除保存文件的语句\n");
-    logging("预计需要运行 14 day 6 h 25 min 39 s = 1232739 s\n");
-    logging("经初步运行的数据分析，单个 n 运行时间服从 0 - 0.000740566 * n + 1.57395e-06 * n^2\n");
-    logging("总计运行时间服从 0 + 0.197321 * n - 0.000455747 * n^2 + 5.28968e-07 * n^3\n");
+    logging("当前算法保存各级 S1_1, 以便从任意 n 开始重新运行，并保存出为整数的 S[n][i][k]\n");
 
     mpq_t **T = read_T("T.bin");
     print_time(&tk[1], tk[0], "read T");
 
-    check_solution_S(T);
+    check_solution_S_with_cache(T, "n_S1_1_cache.bin");
     print_time(&tk[2], tk[1], "check solve S");
 
     free_mpq_array(T, N + 1, K + 1);
@@ -24,27 +22,28 @@ int main() {
     return 0;
 }
 
-void check_solution_S(mpq_t **T) {
+void check_solution_S_with_cache(mpq_t **T, const char *cache_file_name) {
     // 记录开始时间
     clock_t tk[N+2] = {clock()};
 
     // 2 <= n <= N, 1 <= i <= n, 1 <= k <= min(n-1, e ln n + e) 有效
-    // 仅初始化两片空间
+    // 仅初始化一行空间
     // 在初始状态中, S1_1 表示 S[1][*][1]
-    mpq_t *S1_1 = mpq_malloc_init_list(N + 1); // S[1][1][1] = 0
-    // 存放倒数, reciprocal[n] = 1 / n 对 1 <= n <= N 有效
+    int n;
+    mpq_t *S1_1 = read_nth_S1_1(cache_file_name, &n); // S[1][1][1] = 0
     mpq_t *reciprocal = get_reciprocal(N + 1);
     mpq_t Snik;
     mpq_init(Snik);
 
+    int t1 = n - 1;
     logging("\nstep 1: initial finished\n");
-    print_time(&tk[1], tk[0], "");
+    print_time(&tk[t1], tk[0], "");
     logging("\n");
 
-    int n, k, i, max_k;
+    int i, k, max_k;
 
     // 在迭代状态中, S1_1[i], Snik 分别表示 S[n-1][i][1], S[n][i][k]
-    for (n = 2; n <= N; n++) {
+    for (; n <= N; n++) {
         max_k = sup_K(n);
 
         // 1 <= i < n
@@ -79,19 +78,70 @@ void check_solution_S(mpq_t **T) {
         }
 
         logging("n = %d, ", n);
+        save_nth_S1_1(n, S1_1, cache_file_name);
         print_time(&tk[n], tk[n-1], "");
     }
 
     logging("\nstep 2: iteration finished");
-    print_time(&tk[N], tk[1], "step 2");
+    print_time(&tk[N], tk[t1], "step 2");
     logging("\n");
 
     // clear
     free_mpq_list(S1_1, N + 1);
     free_mpq_list(reciprocal, N + 1);
-    mpq_clear(Snik);
 
     logging("\nstep 3: clear finished");
     print_time(&tk[N+1], tk[N], __func__);
     logging("\n");
+}
+
+mpq_t *read_nth_S1_1(const char *file_name, int *ptr_n) {
+    mpq_t *S1_1 = mpq_malloc_init_list(N + 1); // S[1][1][1] = 0
+    FILE *file = fopen(file_name, "rb");
+
+    // 找不到缓存文件，重头开始计算
+    if (!file) {
+        *ptr_n = 2;
+        return S1_1;
+    }
+
+    mpz_t n;
+    mpz_init(n);
+    mpz_read(n, file, "n "); // 读 n
+
+    *ptr_n = (int) mpz_get_ui(n);
+
+    // 读 S1_1
+    // 注意 S1_1 装的是 n - 1 时的数据，因此 i 最大到 *ptr_n - 1
+    int i;
+    for (i = 0; i < *ptr_n; i++) {
+        mpq_read(S1_1[i], file);
+    }
+
+    fclose(file);
+    return S1_1;
+}
+
+void save_nth_S1_1(const int n, const mpq_t *S1_1, const char *file_name) {
+    clock_t tk[2] = {clock()};
+    mpz_t n_mpz;
+    mpz_init(n_mpz);
+    // 注意下一轮循环将要运行的是 n + 1
+    mpz_set_ui(n_mpz, (unsigned long int) n + 1);
+
+    FILE *file = open_file(file_name, "wb");
+
+    // 写入 n + 1
+    if (!mpz_out_raw(file, n_mpz)) {
+        exit(-1);
+    }
+
+    // 写入 S1_1
+    int i;
+    for (i = 0; i <= n; i++) {
+        save_to(file, S1_1[i], i, -1);
+    }
+
+    fclose(file);
+    print_time(&tk[1], tk[0], __func__);
 }
