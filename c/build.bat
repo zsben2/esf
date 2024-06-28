4600b8586c566ba8b4c744203e317337bcb3a57f
zsben2 <zhao_chongbin@yeah.net>
2024-06-28 09:56:33 +0800

    v1.3代码 本版本与前一版本相比，开辟更少的内存空间。前一版本需开辟两块 $N\times K$ 的空间，但本版本仅需开辟 1 行长为 $N$ 的空间和 1 行长为 $K$ 的空间。大大减少了内存的消耗量。但是仍然会将中途计算过的 `S[n][i][k]` 缓存下来，并且计算的算法也没有太大差别。因此硬盘消耗空间与计算时间总体上没有太大的差别。 运行到 n = 1914, S 的缓存文件约有 49 GB 大小。

--- c/build.bat
+++ c/build.bat
@@ -4,5 +4,5 @@ gcc -o "./output/gmp_T_dynamic" "./src/gmp计算T.c" -lgmp
 echo "正在进行静态编译"
 gcc -o "./output/gmp_Si_static" "./src/gmp计算Si.c" -L./lib -lgmp
 gcc -o "./output/gmp_T_static" "./src/gmp计算T.c" -L./lib -lgmp
-del "./output/integer.txt"
+del ".\output\integer.txt"
 pause
