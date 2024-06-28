e2d9f6d8497a02271c62480f5ae9411da419db4b
zsben2 <zhao_chongbin@yeah.net>
2024-06-28 11:05:29 +0800

    v1.5代码 本版本与前一版本相比，新增了从完整的 S 缓存文件中读取数据并校验整数的函数。以便于脱离计算时也可直接读取缓存验证整数。预计总缓存文件大小接近 20 PB 。由于单行内存空间的复用，完整运行所需时间预估需时20天。

--- c/src/gmp计算Si.c
+++ c/src/gmp计算Si.c
@@ -1,21 +1,30 @@
 #include "tools.h"
 
 void check_solution_S_save(mpq_t**, char*);
+void check_S_from_file(char*);
 
 int main() {
     clock_t tk[4] = {clock()};
     mpq_t ***S;
-    logging("当前算法仅开放一行 i 的 N 个空间滚动使用\n");
+    logging("当前算法仅开放一行 i 的空间滚动使用，并放弃使用一个闲置空间\n");
+    logging("预计运行20天，保存20PB的数据\n");
 
-    mpq_t **T = read_T("T.bin");
-    print_time(&tk[1], tk[0], "read T");
+    int re_run = 1;
 
-    check_solution_S_save(T, "S.bin");
-    print_time(&tk[2], tk[1], "check solve S");
+    if (re_run) {
+        mpq_t **T = read_T("T.bin");
+        print_time(&tk[1], tk[0], "read T");
 
-    free_mpq_array(T, N + 1, K + 1);
-    print_time(&tk[3], tk[2], "free T");
+        check_solution_S_save(T, "S.bin");
+        print_time(&tk[2], tk[1], "check solve S");
 
+        free_mpq_array(T, N + 1, K + 1);
+        print_time(&tk[3], tk[2], "free T");
+    } else {
+        check_S_from_file("S.bin");
+    }
+
+    print_time(&tk[3], tk[0], __func__);
     return 0;
 }
 
@@ -27,21 +36,18 @@ void check_solution_S_save(mpq_t **T, char *file_name) {
     // 仅初始化两片空间
     // 在初始状态中, S1_1 表示 S[1][*][1]
     mpq_t *S1_1 = mpq_malloc_init_list(N + 1); // S[1][1][1] = 0
-    // 存放倒数, reciprocal[n] = 1 / n 对 2 <= n <= N 有效
     mpq_t *reciprocal = get_reciprocal(N + 1);
+    mpq_t Snik;
+    mpq_init(Snik);
 
     logging("\nstep 1: initial finished\n");
     print_time(&tk[1], tk[0], "");
     logging("\n");
 
     int n, k, i, max_k;
-    mpq_t Snik;
-    mpq_init(Snik);
-    // 闲置的 0 号空间被用于存储 k-1 时的值
-    mpq_t *pSnik_1 = &S1_1[0];
     FILE *file = open_file(file_name, "wb");
 
-    // 在迭代状态中, S1_1[i], *pSnik_1, Snik 分别表示 S[n-1][i][1], S[n][i][k-1], S[n][i][k]
+    // 在迭代状态中, S1_1[i], Snik 分别表示 S[n-1][i][1], S[n][i][k]
     for (n = 2; n <= N; n++) {
         max_k = sup_K(n);
 
@@ -52,21 +58,17 @@ void check_solution_S_save(mpq_t **T, char *file_name) {
             // check & save S[n][i][1]
             print_if_integer(Snik, n, i, 1, -1);
             save_to(file, Snik, n, i, 1, -1);
-            // 新 S[n-1][i][1] = 现在的 S[n][i][1]
+            // 新 S[n-1][i][1] = 现在的 S[n][i][1] for 1 <= i < n
             mpq_set(S1_1[i], Snik);
-            // 记录 S[n][i][1]
-            mpq_set(*pSnik_1, Snik);
 
             // 2 <= k <= max_k
             for (k = 2; k <= max_k; k++) {
                 // S[n][i][k] = T[n][k] - 1/i * S[n][i][k-1]
-                mpq_mul(Snik, reciprocal[i], *pSnik_1);
+                mpq_mul(Snik, reciprocal[i], Snik);
                 mpq_sub(Snik, T[n][k], Snik);
                 // check & save S[n][i][k] for 2 <= k <= max_k
                 print_if_integer(Snik, n, i, k, -1);
                 save_to(file, Snik, n, i, k, -1);
-                // 新 S[n][i][k-1] = 现在的 S[n][i][k]
-                mpq_set(*pSnik_1, Snik);
             }
         }
 
@@ -100,4 +102,25 @@ void check_solution_S_save(mpq_t **T, char *file_name) {
     logging("\nstep 3: clear finished");
     print_time(&tk[N+1], tk[N], __func__);
     logging("\n");
+}
+
+void check_S_from_file(char *file_name) {
+    clock_t tk[2] = {clock()};
+    FILE *file = open_file(file_name, "rb");
+    int n, i, k, max_k;
+    mpq_t Snik;
+    mpq_init(Snik);
+
+    for (n = 2; n <= N; n++) {
+        max_k = sup_K(N); // <= K
+        for (i = 1; i <= n; i++) {
+            for (k = 1; k <= max_k; k++) {
+                mpq_read(Snik, file);
+                print_if_integer(Snik, n, i, k, -1);
+            }
+        }
+    }
+
+    fclose(file);
+    print_time(&tk[1], tk[0], __func__);
 }
