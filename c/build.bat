f573ac14eff0ffd8080cc736041e62fe8e5ea128
zsben2 <zhao_chongbin@yeah.net>
2024-07-18 00:16:45 +0800

    v2.2 代码 从本版本起，算法 2 也不再使用 T.bin 文件。而 T[n][1] 的计算将先于主循环独立进行，并且仅计算和缓存需要用到的 T。

--- c/build.bat
+++ c/build.bat
@@ -1,10 +1,8 @@
 echo "正在进行动态编译"
 gcc -o "./output/gmp_Si_1_dynamic" "./src/gmp计算Si_1.c" -lgmp
 gcc -o "./output/gmp_Si_2_dynamic" "./src/gmp计算Si_2.c" -lgmp
-gcc -o "./output/gmp_T_dynamic" "./src/gmp计算T.c" -lgmp
 echo "正在进行静态编译"
 gcc -o "./output/gmp_Si_1_static" "./src/gmp计算Si_1.c" -L./lib -lgmp
 gcc -o "./output/gmp_Si_2_static" "./src/gmp计算Si_2.c" -L./lib -lgmp
-gcc -o "./output/gmp_T_static" "./src/gmp计算T.c" -L./lib -lgmp
 del ".\output\integer.txt"
 pause
--- c/run.bat
+++ c/run.bat
@@ -1,5 +1,4 @@
 cd output
 mkdir S1_1
-gmp_T_static.exe
 gmp_Si_1_static.exe
 gmp_Si_2_static.exe
